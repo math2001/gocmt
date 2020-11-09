709d05f8a543b295185b9f9766c7d3d6aed18e56
Mathieu PATUREL <australie.p@gmail.com>
2020-11-09 14:50:59 +1100

    add load check

--- checks/load.go
+++ checks/load.go
@@ -1,11 +1,37 @@
 package checks
 
-import "github.com/math2001/gocmt/cmt"
+import (
+	"github.com/math2001/gocmt/cmt"
+	"github.com/shirou/gopsutil/load"
+)
 
 func Load(
 	cr *cmt.CheckResult,
 	globals map[string]interface{},
 	settings map[string]interface{},
 ) {
-	panic("fail")
+	loadavg, err := load.Avg()
+	if err != nil {
+		cr.AddError(err)
+		return
+	}
+
+	cr.AddItem(&cmt.CheckItem{
+		Name:        "load1",
+		Value:       loadavg.Load1,
+		Description: "CPU Load Average, one minute",
+	})
+
+	cr.AddItem(&cmt.CheckItem{
+		Name:        "load5",
+		Value:       loadavg.Load5,
+		Description: "CPU Load Average, five minutes",
+	})
+
+	cr.AddItem(&cmt.CheckItem{
+		Name:        "load15",
+		Value:       loadavg.Load15,
+		Description: "CPU Load Average, fifteen minutes",
+	})
+
 }
