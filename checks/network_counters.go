c2f403e99526da188c043e3d66ca7bc500e5d9a6
Mathieu PATUREL <australie.p@gmail.com>
2020-11-16 20:16:19 +1100

    add network_counters check

---
+++ checks/network_counters.go
@@ -0,0 +1,41 @@
+package checks
+
+import (
+	"fmt"
+
+	"github.com/math2001/gocmt/cmt"
+	"github.com/shirou/gopsutil/net"
+)
+
+func NetworkCounters(
+	c *cmt.Check,
+	_ map[string]interface{},
+) {
+	interfacesStat, err := net.IOCounters(false)
+	if err != nil {
+		panic(err)
+	}
+
+	statsSum := interfacesStat[0]
+
+	if prevBytesSent, ok := c.DB["prev_bytes_sent"]; ok {
+		c.AddItem(&cmt.CheckItem{
+			Name:        "net_bytes_sent_diff",
+			Value:       statsSum.BytesSent - uint64(prevBytesSent.(float64)),
+			Description: "Number of bytes sent since the last run",
+			Unit:        "bytes",
+		})
+	}
+	c.DB["prev_bytes_sent"] = statsSum.BytesSent
+
+	if prevBytesRecv, ok := c.DB["prev_bytes_recv"]; ok {
+		c.AddItem(&cmt.CheckItem{
+			Name:        "net_bytes_recv_diff",
+			Value:       statsSum.BytesRecv - uint64(prevBytesRecv.(float64)),
+			Description: "Number of bytes recieved since last run",
+			Unit:        "bytes",
+		})
+	}
+	c.DB["prev_bytes_recv"] = statsSum.BytesRecv
+	fmt.Println(c.DB)
+}
