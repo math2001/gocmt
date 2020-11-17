cddbb5cb99d4f49c7de530357a47437a90d1491d
Mathieu PATUREL <australie.p@gmail.com>
2020-11-17 17:18:17 +1100

    add flag to display list of checks
    fix #15

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/math2001/gocmt/cmt"
@@ -21,11 +22,42 @@ func main() {
 	available := flag.Bool(
 		"available", false,
 		"display available entries found for each checks (manual run on target)")
+	listchecks := flag.Bool(
+		"listchecks", false,
+		"display available checks")
 
 	flag.Parse()
 
 	if *available {
 		debugAvailables()
+		return
+	}
+
+	if *listchecks {
+		// print all the names of the checks, sorted
+		names := make([]string, len(allchecks))
+		i := 0
+		for name := range allchecks {
+			names[i] = name
+			i++
+		}
+
+		sort.Slice(names, func(i, j int) bool {
+			return names[i] < names[j]
+		})
+		lineLength := 0
+		for _, name := range names {
+			if lineLength > 80 {
+				lineLength = 0
+				fmt.Println()
+			}
+			lineLength += len(name)
+			fmt.Print(name, " ")
+		}
+		if lineLength != 0 {
+			fmt.Println()
+		}
+
 		return
 	}
 
